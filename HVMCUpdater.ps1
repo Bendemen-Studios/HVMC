1fa85d53791b87992e57bcaa748321f7fe7341b2
JustPetrov <thepetrovshop@gmail.com>
2026-09-09 23:03:54 +0200

    Fix Fabric Loader version in HVMC updater to 0.19.2

--- HVMCUpdater.ps1
+++ HVMCUpdater.ps1
@@ -8,7 +8,7 @@ $ManifestPath = Join-Path $Root 'content-manifest.json'
 $StatePath = Join-Path $Root 'state.json'
 $LogPath = Join-Path $Root 'bootstrapper.log'
 $McVersion = '1.21.11'
-$FabricLoader = '0.18.1'
+$FabricLoader = '0.19.2'
 
 foreach ($dir in @($Root,$MinecraftDir,$MinecraftDir+'\mods',$MinecraftDir+'\config',$MinecraftDir+'\resourcepacks',$MinecraftDir+'\shaderpacks',$MinecraftDir+'\datapacks',$MinecraftDir+'\kubejs')) {
     New-Item -ItemType Directory -Force -Path $dir | Out-Null
