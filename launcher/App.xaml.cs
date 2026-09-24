9d99e7460ca68a9a38315f75380446eb63f5f0a7
JustPetrov <ben@justpetrov.com>
2026-09-24 21:01:45 +0200

    Fix WPF workflow build error

--- launcher/App.xaml.cs
+++ launcher/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text.Json;
 using System.Windows;
+using System.Windows.Controls;
 using WpfMessageBox = System.Windows.MessageBox;
 
 namespace HVMCLauncher;
